79e01ece347daf5548c6370f440733b8d5b5ffcf
John Baek <johnbaek92@gmail.com>
2018-03-05 16:36:35 -0500

    circles render on screen

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
-const d3 = require('d3');
-const startData = require('../data/start_data.json');
+const d3 = require("d3");
 
-document.addEventListener('DOMContentLoaded', function () {
-    d3.json("startData", function(error, data) {
-        debugger
-    });
+document.addEventListener("DOMContentLoaded", function() {
+  d3.json("data/start_data.json", function(error, data) {
+    let chart = d3.select("#chart")
+
+      let circles = chart.selectAll("circle")
+          .data(data)
+          .enter()
+              .append("circle");
+
+      let circleAttrs = circles        
+              .attr("cx", function(d) { return d.age})
+              .attr("cy", function(d) { return d.minutes })
+              .attr("r", 5)
+              .style("fill", "red")
+  });
 });

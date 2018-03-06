8d9669dff48fc871af5cb0873a62514bf5e87d7d
John Baek <johnbaek92@gmail.com>
2018-03-06 17:55:02 -0500

    add highlight functionality

--- bundle.js
+++ bundle.js
@@ -75,15 +75,23 @@
 /*!*********************!*\
   !*** ./js/index.js ***!
   \*********************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
+"use strict";
+__webpack_require__.r(__webpack_exports__);
 const d3 = __webpack_require__(/*! d3 */ "./node_modules/d3/index.js");
+//modify d3
+d3.selection.prototype.moveToFront = function() {
+  return this.each(function() {
+    this.parentNode.appendChild(this);
+  });
+};
 
-let render = document.addEventListener("DOMContentLoaded", function() {
+const renderData = () => {
   d3.json("data/start_data.json", function(error, data) {
-    let width = 790;
-    let height = 590;
+    const width = 790;
+    const height = 590;
     let xAxisVal = d3.select(".x-selector").property("value");
     let yAxisVal = d3.select(".y-selector").property("value");
 
@@ -122,11 +130,17 @@ let render = document.addEventListener("DOMContentLoaded", function() {
       .attr("transform", `translate(-5)`)
       .call(yAxis);
 
+    let tooltip = d3
+        .select("body")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
     let circles = chart
       .selectAll("circle")
       .data(data)
       .enter()
-      .append("circle");
+        .append("circle");
 
     let circleAttrs = circles
       .attr("cx", function(d) {
@@ -135,14 +149,42 @@ let render = document.addEventListener("DOMContentLoaded", function() {
       .attr("cy", function(d) {
         return yScale(d[yAxisVal]);
       })
-      .attr("r", 10)
+      .attr("r", 8)
+      .attr("name", function(d) {
+          return d["name"];
+      })
       .style("fill", function(d) {
         return colorPicker(d);
+      })
+      .style("opacity", 0.7)
+      .on("mouseover", function(d) {
+        tooltip
+          .transition()
+          .duration(200)
+          .style("opacity", 0.9);
+        tooltip
+          .html(d["name"] + "<br/>" + d[xAxisVal] + ` ${xAxisVal}` + "<br/>" + d[yAxisVal] + ` ${yAxisVal}`)
+          .style("left", d3.event.pageX + "px")
+          .style("top", d3.event.pageY - 28 + "px");
+      })
+      .on("mouseout", function(d) {
+        tooltip
+          .transition()
+          .duration(500)
+          .style("opacity", 0);
       });
   });
-});
+};
 
-let colorPicker = function(d) {
+const removeData = function() {
+    let circles = d3.selectAll("circle");
+    circles.remove();
+    let scales = d3.selectAll("g");
+    scales.remove();
+    renderData();
+};
+
+const colorPicker = function(d) {
   switch (d["position"]) {
     case "PG":
       return "red";
@@ -159,6 +201,33 @@ let colorPicker = function(d) {
   }
 };
 
+const highlightPlayer = () => {
+    let selectedPlayer = d3.select(".highlight").property("value").toLowerCase();
+    let circles = d3.selectAll("circle");
+    circles.each(function(circle){
+        if(circle.name.toLowerCase().indexOf(selectedPlayer) !== -1){
+            let that = d3.select(this);
+            that.moveToFront();
+            that.attr("stroke-width", 50)
+                .attr("stroke", "#ff00cb")
+                .style("opacity", 1);
+        } else {
+            d3.select(this).style("opacity", 0.3);
+        }
+    });
+};
+
+const reRenderData = function () {
+    removeData();
+    renderData();
+};
+
+document.addEventListener("DOMContentLoaded", renderData);
+document.querySelector(".highlight").addEventListener("change", highlightPlayer);
+document.querySelector(".x-selector").addEventListener("change", reRenderData);
+document.querySelector(".y-selector").addEventListener("change", reRenderData);
+
+/* harmony default export */ __webpack_exports__["default"] = (renderData);
 
 /***/ }),
 
--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 const d3 = require("d3");
+//modify d3
+d3.selection.prototype.moveToFront = function() {
+  return this.each(function() {
+    this.parentNode.appendChild(this);
+  });
+};
 
-let render = document.addEventListener("DOMContentLoaded", function() {
+const renderData = () => {
   d3.json("data/start_data.json", function(error, data) {
-    let width = 790;
-    let height = 590;
+    const width = 790;
+    const height = 590;
     let xAxisVal = d3.select(".x-selector").property("value");
     let yAxisVal = d3.select(".y-selector").property("value");
 
@@ -42,11 +48,17 @@ let render = document.addEventListener("DOMContentLoaded", function() {
       .attr("transform", `translate(-5)`)
       .call(yAxis);
 
+    let tooltip = d3
+        .select("body")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
     let circles = chart
       .selectAll("circle")
       .data(data)
       .enter()
-      .append("circle");
+        .append("circle");
 
     let circleAttrs = circles
       .attr("cx", function(d) {
@@ -55,14 +67,42 @@ let render = document.addEventListener("DOMContentLoaded", function() {
       .attr("cy", function(d) {
         return yScale(d[yAxisVal]);
       })
-      .attr("r", 10)
+      .attr("r", 8)
+      .attr("name", function(d) {
+          return d["name"];
+      })
       .style("fill", function(d) {
         return colorPicker(d);
+      })
+      .style("opacity", 0.7)
+      .on("mouseover", function(d) {
+        tooltip
+          .transition()
+          .duration(200)
+          .style("opacity", 0.9);
+        tooltip
+          .html(d["name"] + "<br/>" + d[xAxisVal] + ` ${xAxisVal}` + "<br/>" + d[yAxisVal] + ` ${yAxisVal}`)
+          .style("left", d3.event.pageX + "px")
+          .style("top", d3.event.pageY - 28 + "px");
+      })
+      .on("mouseout", function(d) {
+        tooltip
+          .transition()
+          .duration(500)
+          .style("opacity", 0);
       });
   });
-});
+};
 
-let colorPicker = function(d) {
+const removeData = function() {
+    let circles = d3.selectAll("circle");
+    circles.remove();
+    let scales = d3.selectAll("g");
+    scales.remove();
+    renderData();
+};
+
+const colorPicker = function(d) {
   switch (d["position"]) {
     case "PG":
       return "red";
@@ -78,3 +118,31 @@ let colorPicker = function(d) {
       return "black";
   }
 };
+
+const highlightPlayer = () => {
+    let selectedPlayer = d3.select(".highlight").property("value").toLowerCase();
+    let circles = d3.selectAll("circle");
+    circles.each(function(circle){
+        if(circle.name.toLowerCase().indexOf(selectedPlayer) !== -1){
+            let that = d3.select(this);
+            that.moveToFront();
+            that.attr("stroke-width", 50)
+                .attr("stroke", "#ff00cb")
+                .style("opacity", 1);
+        } else {
+            d3.select(this).style("opacity", 0.3);
+        }
+    });
+};
+
+const reRenderData = function () {
+    removeData();
+    renderData();
+};
+
+document.addEventListener("DOMContentLoaded", renderData);
+document.querySelector(".highlight").addEventListener("change", highlightPlayer);
+document.querySelector(".x-selector").addEventListener("change", reRenderData);
+document.querySelector(".y-selector").addEventListener("change", reRenderData);
+
+export default renderData;

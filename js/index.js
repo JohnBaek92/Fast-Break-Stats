6452321ad14a9ad80e5cf53fcdff7c931716f2df
John Baek <johnbaek92@gmail.com>
2018-03-06 14:53:48 -0500

    scales render on screen

--- js/index.js
+++ js/index.js
@@ -2,24 +2,39 @@ const d3 = require("d3");
 
 let render = document.addEventListener("DOMContentLoaded", function() {
   d3.json("data/start_data.json", function(error, data) {
-    let chart = d3.select("#chart");
-    let width = 790;
-    let height = 590;
-    let xAxisVal = d3.select(".x-selector").property("value");
-    let yAxisVal = d3.select(".y-selector").property("value");
-    let xScale = d3.scaleLinear()
-                  .domain(d3.extent(data, function(d) { return d[xAxisVal]}))
-                  .range([10, width]);
-    let yScale = d3.scaleLinear()
-                  .domain(d3.extent(data, function(d) { return d[yAxisVal]}))
-                  .range([10, height]);
+      let width = 790;
+      let height = 590;
+      let xAxisVal = d3.select(".x-selector").property("value");
+      let yAxisVal = d3.select(".y-selector").property("value");
+
+      let xScale = d3.scaleLinear()
+      .domain(d3.extent(data, function(d) { return d[xAxisVal]}))
+      .range([10, width - 50]);
+
+      let yScale = d3.scaleLinear()
+      .domain(d3.extent(data, function(d) { return d[yAxisVal]}))
+      .range([10, height - 50]);
+      
+      let xAxis = d3.axisBottom().scale(xScale);
+      let yAxis = d3.axisLeft().scale(yScale);
+      let chart = d3.select("#chart")
+                    .append("g")
+                    .attr("transform", "translate(50)")
+
+    chart.append("g")
+    .attr("transform", `translate(0, 580)`).call(xAxis);
+
+    chart.append("g")
+    .attr("transform", `translate(0)`).call(yAxis);
+
+      
 
     let circles = chart
       .selectAll("circle")
       .data(data)
       .enter()
         .append("circle");
-
+    
     let circleAttrs = circles
       .attr("cx", function(d) {
         return xScale(d[xAxisVal]);
@@ -27,7 +42,7 @@ let render = document.addEventListener("DOMContentLoaded", function() {
       .attr("cy", function(d) {
         return yScale(d[yAxisVal]);
       })
-      .attr("r", 5)
+      .attr("r", 10)
       .style("fill", function(d) {
           return colorPicker(d);
       });
@@ -49,7 +64,7 @@ let colorPicker = function(d) {
             return "purple";
             break;
         case "C":
-            return "orange"
+            return "orange";
         default: 
             return "black";        
     }

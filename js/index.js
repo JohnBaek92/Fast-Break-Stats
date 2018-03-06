3191510f014d5290fdabea396eadc45609f2e686
John Baek <johnbaek92@gmail.com>
2018-03-06 11:41:28 -0500

    add select menu for y and x axes in index.html

--- js/index.js
+++ js/index.js
@@ -2,17 +2,26 @@ const d3 = require("d3");
 
 document.addEventListener("DOMContentLoaded", function() {
   d3.json("data/start_data.json", function(error, data) {
-    let chart = d3.select("#chart")
+    let chart = d3.select("#chart");
 
-      let circles = chart.selectAll("circle")
-          .data(data)
-          .enter()
-              .append("circle");
+    let circles = chart
+      .selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle");
 
-      let circleAttrs = circles        
-              .attr("cx", function(d) { return d.age})
-              .attr("cy", function(d) { return d.minutes })
-              .attr("r", 5)
-              .style("fill", "red")
+    let widthScale = 
+
+    let circleAttrs = circles
+      .attr("cx", function(d) {
+        return d.age;
+      })
+      .attr("cy", function(d) {
+        return d.minutes;
+      })
+      .attr("r", 5)
+      .style("fill", "red");
   });
 });
+
+function setXAxis(option)

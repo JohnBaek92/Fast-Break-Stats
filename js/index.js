96752a2d016dd09f8082ca937930e2d59beaaa4c
John Baek <johnbaek92@gmail.com>
2018-03-06 12:09:40 -0500

    add x scale and y scale

--- js/index.js
+++ js/index.js
@@ -1,27 +1,33 @@
 const d3 = require("d3");
 
-document.addEventListener("DOMContentLoaded", function() {
+let render = document.addEventListener("DOMContentLoaded", function() {
   d3.json("data/start_data.json", function(error, data) {
     let chart = d3.select("#chart");
+    let width = 800;
+    let height = 600;
+    let xAxisVal = d3.select(".x-selector").property("value");
+    let yAxisVal = d3.select(".y-selector").property("value");
+    let xScale = d3.scaleLinear()
+                  .domain(d3.extent(data, function(d) { return d[xAxisVal]}))
+                  .range([10, width]);
+    let yScale = d3.scaleLinear()
+                  .domain(d3.extent(data, function(d) { return d[yAxisVal]}))
+                  .range([10, height]);
 
     let circles = chart
       .selectAll("circle")
       .data(data)
       .enter()
-      .append("circle");
-
-    let widthScale = 
+        .append("circle");
 
     let circleAttrs = circles
       .attr("cx", function(d) {
-        return d.age;
+        return xScale(d[xAxisVal]);
       })
       .attr("cy", function(d) {
-        return d.minutes;
+        return yScale(d[yAxisVal]);
       })
       .attr("r", 5)
       .style("fill", "red");
   });
 });
-
-function setXAxis(option)
